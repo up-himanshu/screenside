5d37576a432ab4a416195620fba9966502091a66
Antoniohdz99 <ricardo_antonyo@hotmail.com>
2019-06-22 20:45:49 -0500

    Datos barco

--- src/app/componentes/generales/menu/juego/juego.component.ts
+++ src/app/componentes/generales/menu/juego/juego.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/interface/generales/usuario';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
 import { JugadorService } from 'src/app/servicios/jugador.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { promise } from 'protractor';
 
 
 @Component({
@@ -10,21 +12,76 @@ import { JugadorService } from 'src/app/servicios/jugador.service';
   styleUrls: ['./juego.component.css']
 })
 export class JuegoComponent implements OnInit {
-  player: Usuario[] = [];
-  constructor(private userService: UsuarioService, private playerService: JugadorService) { }
+  public cantidad =0;
+  public cantidadPadingleft =0;
+  player: Usuario[] = [];public NumeroPantalla;
+  constructor(private userService: UsuarioService, private playerService: JugadorService) {
+    this.NumeroPantalla=0;
+   }
 public datos:any
-public dato2s:any;
+public cargando=false;
+
   ngOnInit() {  
     
     this.playerService.conectar();
-    this.playerService.currentMessage.subscribe(isOpen => {
-      this.datos = JSON.stringify(isOpen);
-    });
+    this.playerService.currentMessage.subscribe(isOpen  => {
+
+      this.datos = isOpen ;
+      if(isOpen.length != 0 )
+      {
+          this.Pantalla();
+      }
       
 
-       
+    });
+
+    
+      this.NumeroPantalla=0;
+    
+      
     
   }
+
+  
+
+
+
+  private Inicio_partida (datos_partida)
+  {
+    if (datos_partida[0].pantallaActiva==0)
+    {
+       alert("Esperando partida");
+    }
+    else
+    {
+        if(this.NumeroPantalla != this.datos[0].pantallaActiva)
+        {
+          this.Replay();
+      
+        }
+    }
+  } 
+  
+   public Pantalla () {
+   
+
+  
+      if( this.datos[1].Pantalla1==true && this.NumeroPantalla ==0)
+      {
+        this.NumeroPantalla =2;
+        this.datos[1].Patalla2=true;
+        this.datos[0].pantallaActiva=1;
+        this.Inicio_partida(this.datos);
+      }
+      else
+      {
+        this.NumeroPantalla =1;
+        this.datos[1].Patalla1=true;
+       
+      }
+   
+  }
+
   
   cerrar(){
     alert(this.datos);
@@ -32,4 +89,49 @@ public dato2s:any;
   }
 
   
+  public Replay()
+  {
+  
+    var box = document.getElementById('boat');
+     this.cantidad =0;
+     this.cantidadPadingleft =0;
+     box.style.left='0px';
+     box.style.paddingLeft='0px';
+     var x =setInterval(()=>{
+  
+  
+      var box = document.getElementById('boat');
+      
+  
+      if(!(screen.width-199>=this.cantidad))
+      {
+  
+        if(screen.width>=this.cantidad)
+        {
+  
+          box.style.paddingLeft= this.cantidadPadingleft + "px";
+          this.cantidadPadingleft++;
+          this.cantidad++; 
+        }
+        else
+        {
+            clearInterval(x);
+        
+          }
+         
+  
+        
+         
+      }
+      else 
+      {
+        box.style.left=  this.cantidad + "px";
+          this.cantidad++;
+      }
+      
+    },10);
+    
+  }
+
+
 }
